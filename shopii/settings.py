e40a713c53ab5b37e211b64b6fbe2c08f9934d0f
Hữu Lợi <trloi17it@gmail.com>
2021-10-17 11:20:38 +0700

    Update settings.py

--- shopii/settings.py
+++ shopii/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = os.path.dirname(os.path.dirname(os.path.abspath('static')))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # media files configuration
